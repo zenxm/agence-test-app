1b2d12095fddd956cb039907df54dcb430516c0b
Diogenes <diogenes.hern@gmail.com>
2019-09-10 17:01:15 -0400

    changed api for heroku

--- client/config/config.js
+++ client/config/config.js
@@ -4,4 +4,4 @@ export const APP_HOST = process.env.APP_HOST || 'agence-test-horoku.herokuapp.co
 export const APP_PORT = process.env.PORT || 5000;
 export const HOST = `${APP_HOST}:${APP_PORT}/`;
 
-export const API_URL = `https://${HOST}${apiPath}`;
+export const API_URL = `https://${APP_HOST}/${apiPath}`;
