c66434a32e224668b5cff6f936ff9f2e739980bc
Diogenes <diogenes.hern@gmail.com>
2019-09-10 12:51:04 -0400

    configs hardcoded

--- server/config/database.js
+++ server/config/database.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 export default {
   client: process.env.DB_CLIENT || 'mysql',
   connection: {
-    host: process.env.DB_HOST || 'sql3.freemysqlhosting.net',
-    user: process.env.DB_USER || 'sql9304683',
-    password: process.env.DB_PASSWORD || 'VHifcpUgUT',
-    database: process.env.DB_NAME || 'sql3206184',
+    host: process.env.DB_HOST || 'sql10.freemysqlhosting.net',
+    user: process.env.DB_USER || 'sql10304731',
+    password: process.env.DB_PASSWORD || 'hMseRutHqp',
+    database: process.env.DB_NAME || 'sql10304731',
     charset: 'utf8mb4',
     // socketPath: process.env.SOCKET_PATH,
   },
