23caededd9eb9f636decbfdf58cbf9048c8781e7
Diogenes <diogenes.hern@gmail.com>
2019-09-10 14:20:48 -0400

    configs host production

--- server/app.js
+++ server/app.js
@@ -11,13 +11,13 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack/webpack.config.dev';
 
-if (process.env.NODE_ENV === 'development') {
-  const compiler = webpack(webpackConfig);
-  app.use(
-    webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }),
-  );
-  app.use(webpackHotMiddleware(compiler));
-}
+// if (process.env.NODE_ENV === 'development') {
+const compiler = webpack(webpackConfig);
+app.use(
+  webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }),
+);
+app.use(webpackHotMiddleware(compiler));
+// }
 
 // Swagger API documentation
 app.get('/swagger.json', (req, res) => {
